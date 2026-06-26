09abb16af41a5290f0b3f2b8f3c6474612994e7d
Sakshi patwa <patwasakshi46@gmail.com>
2026-06-26 23:12:57 +0530

    Add files via upload

---
+++ 04-operaters.py
@@ -0,0 +1,47 @@
+#aritehcmitic operster 
+
+a = 34
+
+b = 4
+
+c = a+b 
+print(c)
+
+
+#assemnt operatre
+a = 4 - 2
+b -=3
+
+
+print(b)
+
+#comperision operaters
+
+d = 5<4
+print(d)
+
+e = 5>4
+print(e)
+
+f = 5==5
+print(f)
+
+
+#logical operaters
+
+g = True or False 
+# Truth table of or 
+print("TRUE OR FALSE IS" , True or False)
+print("TRUE OR true IS" , True or True)
+print("FALSE OR FALSE IS" , False or True)
+print("FALSE OR FALSE IS" , False or False)
+
+# Truth table of 'and'
+
+print("TRUE and FALSE IS" , True and False)
+print("TRUE and true IS" , True and True)
+print("FALSE and FALSE IS" , False and True)
+print("FALSE and FALSE IS" , False and False)
+
+
+
---
+++ 05-types.py
@@ -0,0 +1,21 @@
+a  = 31 
+t= type(a)
+print(t)
+
+
+b = "sakshi"
+t = type(b)
+
+print(t)
+
+c = 12.33
+t = type(c)
+
+print(t)
+
+
+d = "32.2"
+e = float(d)
+t = type(d)
+
+print(t)
